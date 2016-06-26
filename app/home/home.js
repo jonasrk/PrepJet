e57905cd1946ecd1cd177431414c352a23f573fc
Corinne Petroschke <corinnepetroschke@Corinnes-MacBook-Pro.local>
2016-06-26 08:51:04 +0200

    split values with split at first delimiter

--- app/home/home.js
+++ app/home/home.js
@@ -1,4 +1,4 @@
-function displayField(){
+function displayFieldDelimiter(){
     if (document.getElementById('delimiter_options').value == "custom_delimiter"){
         $('#delimiter_beginning').show();
     }
@@ -11,16 +11,48 @@ function displayField(){
     Office.initialize = function (reason) {
         jQuery(document).ready(function () {
             app.initialize();
+            fillColumn();
 
             $('#delimiter_beginning').hide();
 
             $(".dropdown_table").Dropdown();
-            $(".ms-TextField").TextField();
+            //$(".ms-TextField").TextField();
 
-            $('#split_value').click(splitValue);
+            $('#split_Value').click(splitValue);
 
         });
     };
+
+
+    function fillColumn(){
+
+        Excel.run(function (ctx) {
+
+            var worksheet = ctx.workbook.worksheets.getActiveWorksheet();
+            var range_all = worksheet.getRange();
+            var range = range_all.getUsedRange();
+
+            range.load('text');
+            return ctx.sync().then(function() {
+                for (var i = 0; i < range.text[0].length; i++) {
+
+                    var el = document.createElement("option");
+                    el.value = range.text[0][i];
+                    el.textContent = range.text[0][i];
+                    document.getElementById("column_options").appendChild(el);
+                }
+
+                $(".dropdown_table_col").Dropdown();
+            });
+
+        }).catch(function(error) {
+            console.log("Error: " + error);
+            if (error instanceof OfficeExtension.Error) {
+                console.log("Debug info: " + JSON.stringify(error.debugInfo));
+            }
+        });
+
+    }
 
 
     function splitValue() {
@@ -31,21 +63,18 @@ function displayField(){
             var range = range_all.getUsedRange();
             var selected_identifier = document.getElementById('column_options').value;
 
-            var split_beginning = document.getElementById('beginning_options').value;
-            if (document.getElementById('beginning_options').value == "custom_delimiter"){
-                var split_beginning = document.getElementById('delimiter_input').value;
+            var delimiter_type = document.getElementById('delimiter_options').value;
+            if (delimiter_type == "custom_delimiter"){
+                var delimiter_type = document.getElementById('delimiter_input').value;
             }
-            else {
-                var split_beginning = document.getElementById('delimiter_options').value;
+            if (delimiter_type == "whitespace") {
+                delimiter_type = " ";
             }
-            if (split_beginning == "whitespace") {
-                split_beginning = " ";
+            if (delimiter_type == "comma") {
+                delimiter_type = ",";
             }
-            if (split_beginning == "comma") {
-                split_beginning = ",";
-            }
-            if (split_beginning == "semikolon") {
-                split_beginning = ";";
+            if (delimiter_type == "semikolon") {
+                delimiter_type = ";";
             }
 
 
@@ -67,26 +96,30 @@ function displayField(){
                 }
 
                 var act_worksheet = ctx.workbook.worksheets.getActiveWorksheet();
-                var rangeaddress = getCharFromNumber(header + 2) + 1;
-                var range_insert = ctx.workbook.worksheets.getActiveWorksheet().getRange(rangeaddress);
-                range_insert.insert("Right");
+                //var rangeaddress = getCharFromNumber(header + 2) + 1;
+                //var range_insert = ctx.workbook.worksheets.getActiveWorksheet().getRange(rangeaddress);
+                //range_insert.insert("Right");
 
                 for (var i = 1; i < range.text.length; i++) {
 
                     //todo loop für alle positions des delimiters
-                    var position1 = range.text[i][header].indexOf(split_beginning);
-                    var position2 = range.text[i][header].indexOf(split_end);
+                    var position1 = range.text[i][header].indexOf(delimiter_type);
 
-                    var extractedValue = range.text[i][header].substring(position1, position2);
+                    var splitValue1 = range.text[i][header].substring(0, position1);
+                    var splitValue2 = range.text[i][header].substring(position1, range.text[i][header].length);
+
                     var column_char = getCharFromNumber(header + 2)
                     var sheet_row = i + 1;
 
-                    //var rangeaddress = column_char + sheet_row;
-                    //var range_insert = ctx.workbook.worksheets.getActiveWorksheet().getRange(rangeaddress);
-                    //range_insert.insert("Right");
+                    var rangeaddress = column_char + sheet_row;
+                    var range_insert = ctx.workbook.worksheets.getActiveWorksheet().getRange(rangeaddress);
+                    range_insert.insert("Right");
+                    range_insert.insert("Right")
                     //todo erstmal am Ende einfügen
-                    addContentToWorksheet(act_worksheet, rangeaddress, extractedValue);
+                    addContentToWorksheet(act_worksheet, column_char + sheet_row, splitValue1);
+                    addContentToWorksheet(act_worksheet, getCharFromNumber(header + 3) + sheet_row, splitValue2);
 
+                    header_col = i
                     console.log(column_char + sheet_row)
                 }
 
