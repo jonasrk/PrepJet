69c6bada3c282a5f799f43d277e495d5c895eb0c
Jonas <jonas.kemper.biz@gmail.com>
2016-06-27 11:11:19 +0200

    calculating quartiles

--- app/home/outlier.js
+++ app/home/outlier.js
@@ -65,13 +65,34 @@
                 for (var k = 0; k < range.text[0].length; k++){
                     if (selected_column == range.text[0][k]){
 
+                        var values = [];
+
                         for (var j = 1; j < range.text.length; j++){
 
-
-                            console.log(range.text[j][k]);
-
+                            values.push(range.text[j][k]);
 
                         }
+
+                        // High outliers are anything beyond the 3rd quartile + 1.5 * the inter-quartile range (IQR)
+                        // Low outliers are anything beneath the 1st quartile - 1.5 * IQR
+
+                        values.sort();
+
+                        var Q1, Q2, Q3 = 0;
+
+                        var q1Arr = (values.length % 2 == 0) ? values.slice(0, (values.length / 2)) : values.slice(0, Math.floor(values.length / 2));
+                        var q2Arr =  values;
+                        var q3Arr = (values.length % 2 == 0) ? values.slice((values.length / 2), values.length) : values.slice(Math.ceil(values.length / 2), values.length);
+                        Q1 = medianX(q1Arr);
+                        Q2 = medianX(q2Arr);
+                        Q3 = medianX(q3Arr);
+                        function medianX(medianArr) {
+                            var count = medianArr.length;
+                            var median = (count % 2 == 0) ? (medianArr[(medianArr.length/2) - 1] + medianArr[(medianArr.length / 2)]) / 2:medianArr[Math.floor(medianArr.length / 2)];
+                            return median;
+                        }
+                        console.log(q1Arr, q2Arr, q3Arr);
+                        console.log(Q1,Q2,Q3);
 
                     }
                 }
