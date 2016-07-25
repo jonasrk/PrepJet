2bed1c5060d094e5d592395712f599e58f66885f
Corinne <corinne.petroschke@patya.de>
2016-07-25 23:39:09 +0200

    color of duplicate header

--- app/home/trim_spaces.js
+++ app/home/trim_spaces.js
@@ -112,8 +112,9 @@
                     for (var run2 = run + 1; run2 < range.text[0].length; run2++) {
                         if (range.text[0][run] == range.text[0][run2]) {
                             $("#showEmbeddedDialog").show();
+                            highlightContentInWorksheet(worksheet, getCharFromNumber(run) + 1, '#EA7F04');
+                            highlightContentInWorksheet(worksheet, getCharFromNumber(run2) + 1, '#EA7F04');
                             Office.context.document.settings.set('same_header_trim', true);
-                            break;
                         }
                     }
                 }
