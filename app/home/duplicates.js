a31ae1378c499c5144a1d2708e38bc4292b810a5
Corinne <corinne.petroschke@patya.de>
2016-07-27 17:47:40 +0200

    change header handler added to duplicates

--- app/home/duplicates.js
+++ app/home/duplicates.js
@@ -5,6 +5,7 @@
     Office.initialize = function (reason) {
         jQuery(document).ready(function () {
 
+            Office.context.document.settings.set('same_header_duplicates', false);
             Office.context.document.settings.set('last_clicked_function', "duplicates.html");
             if (Office.context.document.settings.get('prepjet_loaded_before') == null) {
                 Office.context.document.settings.set('prepjet_loaded_before', true);
@@ -15,8 +16,83 @@
             app.initialize();
             populateCheckboxes();
 
+            if (Office.context.document.settings.get('same_header_duplicates') == false) {
+                $("#showEmbeddedDialog").hide();
+            }
+
             $('#bt_detect_duplicates').click(detectDuplicates);
             $('#checkbox_all').click(checkCheckbox);
+
+            // Hides the dialog.
+            document.getElementById("buttonClose").onclick = function () {
+                $("#showEmbeddedDialog").hide();
+            }
+
+            // Performs the action and closes the dialog.
+            document.getElementById("buttonOk").onclick = function () {
+                $("#showEmbeddedDialog").hide();
+            }
+
+            Excel.run(function (ctx) {
+
+                var myBindings = Office.context.document.bindings;
+                var worksheetname = ctx.workbook.worksheets.getActiveWorksheet();
+
+                worksheetname.load('name')
+
+                return ctx.sync().then(function() {
+
+                    function bindFromPrompt() {
+
+                        var myBindings = Office.context.document.bindings;
+                        var name_worksheet = worksheetname.name;
+                        var myAddress = name_worksheet.concat("!1:1");
+
+                        myBindings.addFromNamedItemAsync(myAddress, "matrix", {id:'myBinding'}, function (asyncResult) {
+                            if (asyncResult.status == Office.AsyncResultStatus.Failed) {
+                                write('Action failed. Error: ' + asyncResult.error.message);
+                            } else {
+                                write('Added new binding with type: ' + asyncResult.value.type + ' and id: ' + asyncResult.value.id);
+
+                                function addHandler() {
+                                    Office.select("bindings#myBinding").addHandlerAsync(
+                                        Office.EventType.BindingDataChanged, dataChanged);
+                                }
+
+                                addHandler();
+                                displayAllBindings();
+
+                            }
+                        });
+                    }
+
+                bindFromPrompt();
+
+                function displayAllBindings() {
+                    Office.context.document.bindings.getAllAsync(function (asyncResult) {
+                        var bindingString = '';
+                        for (var i in asyncResult.value) {
+                            bindingString += asyncResult.value[i].id + '\n';
+                        }
+                    });
+                }
+
+                function dataChanged(eventArgs) {
+                    window.location = "duplicates.html";
+                }
+
+                // Function that writes to a div with id='message' on the page.
+                function write(message){
+                    console.log(message);
+                }
+
+                });
+            }).catch(function(error) {
+                console.log("Error: " + error);
+                if (error instanceof OfficeExtension.Error) {
+                    console.log("Debug info: " + JSON.stringify(error.debugInfo));
+                }
+            });
 
         });
     };
@@ -33,6 +109,18 @@
             range.load('address');
             range.load('text');
             return ctx.sync().then(function() {
+
+                for (var run = 0; run < range.text[0].length - 1; run++) {
+                    for (var run2 = run + 1; run2 < range.text[0].length; run2++) {
+                        if (range.text[0][run] == range.text[0][run2]) {
+                            $("#showEmbeddedDialog").show();
+                            highlightContentInWorksheet(worksheet, getCharFromNumber(run) + 1, '#EA7F04');
+                            highlightContentInWorksheet(worksheet, getCharFromNumber(run2) + 1, '#EA7F04');
+                            Office.context.document.settings.set('same_header_duplicates', true);
+                        }
+                    }
+                }
+
                 for (var i = 0; i < range.text[0].length; i++) { // .text[0] is the first row of a range
                     if (range.text[0][i] != ""){
                         addNewCheckboxToContainer (range.text[0][i], "duplicates_column_checkbox" ,"checkboxes_duplicates");
