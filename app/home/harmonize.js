00ce2f24d7575af59996a541ad05c30149607fc1
Corinne <corinne.petroschke@patya.de>
2016-08-01 20:10:05 +0200

    new error message in harmonize and inconsistencies

--- app/home/harmonize.js
+++ app/home/harmonize.js
@@ -20,13 +20,11 @@
             $(".harmonize_drop").Dropdown();
             $('#harmonize').click(harmonize);
             $('#checkbox_all').click(checkCheckbox);
+            $('#buttonOk').click(highlightHeader);
 
             //Show and hide error message if column have same header name
             document.getElementById("buttonClose").onclick = function () {
-                $("#showEmbeddedDialog").hide();
-            }
-            document.getElementById("buttonOk").onclick = function () {
-                $("#showEmbeddedDialog").hide();
+                document.getElementById('showEmbeddedDialog').style.visibility = 'hidden';
             }
 
 
@@ -112,6 +110,39 @@
     };
 
 
+    function highlightHeader() {
+
+        Excel.run(function (ctx) {
+
+            var worksheet = ctx.workbook.worksheets.getActiveWorksheet();
+            var range_all = worksheet.getRange();
+            var range = range_all.getUsedRange();
+
+            range.load('text');
+
+            return ctx.sync().then(function() {
+
+                for (var run = 0; run < range.text[0].length - 1; run++) {
+                    for (var run2 = run + 1; run2 < range.text[0].length; run2++) {
+                        if (range.text[0][run] == range.text[0][run2]) {
+                            document.getElementById('showEmbeddedDialog').style.visibility = 'hidden';
+                            highlightContentInWorksheet(worksheet, getCharFromNumber(run) + 1, '#EA7F04');
+                            highlightContentInWorksheet(worksheet, getCharFromNumber(run2) + 1, '#EA7F04');
+                        }
+                    }
+                }
+
+            });
+
+        }).catch(function(error) {
+            console.log("Error: " + error);
+            if (error instanceof OfficeExtension.Error) {
+                console.log("Debug info: " + JSON.stringify(error.debugInfo));
+            }
+        });
+    }
+
+
     function checkCheckbox() {
 
         Excel.run(function (ctx) {
@@ -171,8 +202,6 @@
                     for (var run2 = run + 1; run2 < range.text[0].length; run2++) {
                         if (range.text[0][run] == range.text[0][run2]) {
                             document.getElementById('showEmbeddedDialog').style.visibility = 'visible';
-                            highlightContentInWorksheet(worksheet, getCharFromNumber(run) + 1, '#EA7F04');
-                            highlightContentInWorksheet(worksheet, getCharFromNumber(run2) + 1, '#EA7F04');
                             Office.context.document.settings.set('same_header_harmonize', true);
                         }
                     }
