7118e3b44d1e1ebfc341fec1f5d9693e480bbfaf
Jonas <jonas.kemper.biz@gmail.com>
2016-08-09 12:52:51 +0200

    referencing api dupe function

--- app/home/duplicates_api.js
+++ app/home/duplicates_api.js
@@ -1,10 +1,5 @@
-function fuzzyPro() {
-    document.getElementById('showEnterprise').style.visibility = 'visible';
-    document.getElementById('fuzzymatch').checked = false;
-}
-
 (function () {
-    // 'use strict';
+    'use strict';
     var sorted_rows = [];
     // The initialize function must be run each time a new page is loaded
     Office.initialize = function (reason) {
@@ -24,16 +19,20 @@ function fuzzyPro() {
 
             $('#bt_detect_duplicates').click(detectDuplicates);
             $('#checkbox_all').click(checkCheckbox);
-            $('#buttonOk').click(highlightHeader);
 
 
             //show and hide error message when columns have same header name
             document.getElementById("buttonClose").onclick = function () {
-                document.getElementById('showEmbeddedDialog').style.visibility = 'hidden';
+                $("#showEmbeddedDialog").hide();
+            }
+            document.getElementById("buttonOk").onclick = function () {
+                $("#showEmbeddedDialog").hide();
             }
 
-
             //show and hide message about PrepJet Pro when hovering over fuzzy matching
+            document.getElementById("fuzzy_match").onmouseover = function () {
+                document.getElementById('showEnterprise').style.visibility = 'visible';
+            }
             document.getElementById('buttonCloseEnterprise').onclick = function () {
                 document.getElementById('showEnterprise').style.visibility = 'hidden';
             }
@@ -49,134 +48,87 @@ function fuzzyPro() {
                 document.getElementById('helpCallout').style.visibility = 'hidden';
             }
 
-            document.getElementById("refresh_icon").onclick = function () {
-                window.location = "duplicates.html";
-            }
 
-            //hide result message
-            document.getElementById("resultClose").onclick = function () {
-                document.getElementById('resultDialog').style.visibility = 'hidden';
-                window.location = "duplicates.html";
-            }
-            document.getElementById("resultOk").onclick = function () {
-                document.getElementById('resultDialog').style.visibility = 'hidden';
-                window.location = "duplicates.html";
-            }
+            Excel.run(function (ctx) {
 
+                var myBindings = Office.context.document.bindings;
+                var worksheetname = ctx.workbook.worksheets.getActiveWorksheet();
 
-            /*Excel.run(function (ctx) {
+                worksheetname.load('name')
 
-             var myBindings = Office.context.document.bindings;
-             var worksheetname = ctx.workbook.worksheets.getActiveWorksheet();
+                return ctx.sync().then(function() {
 
-             worksheetname.load('name')
+                    Office.context.document.addHandlerAsync("documentSelectionChanged", myViewHandler, function(result){}
+                    );
 
-             return ctx.sync().then(function() {
-
-             Office.context.document.addHandlerAsync("documentSelectionChanged", myViewHandler, function(result){}
-             );
-
-             // Event handler function for changing the worksheet.
-             function myViewHandler(eventArgs){
-             Excel.run(function (ctx) {
-             var selectedSheet = ctx.workbook.worksheets.getActiveWorksheet();
-             selectedSheet.load('name');
-             return ctx.sync().then(function () {
-             if (selectedSheet.name != worksheetname.name) {
-             window.location = "duplicates.html"
-             }
-             });
-             });
-             }
+                    // Event handler function for changing the worksheet.
+                    function myViewHandler(eventArgs){
+                        Excel.run(function (ctx) {
+                            var selectedSheet = ctx.workbook.worksheets.getActiveWorksheet();
+                            selectedSheet.load('name');
+                            return ctx.sync().then(function () {
+                                if (selectedSheet.name != worksheetname.name) {
+                                    window.location = "duplicates.html"
+                                }
+                            });
+                        });
+                    }
 
 
-             function bindFromPrompt() {
+                    function bindFromPrompt() {
 
-             var myBindings = Office.context.document.bindings;
-             var name_worksheet = worksheetname.name;
-             var myAddress = name_worksheet.concat("!1:1");
+                        var myBindings = Office.context.document.bindings;
+                        var name_worksheet = worksheetname.name;
+                        var myAddress = name_worksheet.concat("!1:1");
 
-             myBindings.addFromNamedItemAsync(myAddress, "matrix", {id:'myBinding'}, function (asyncResult) {
-             if (asyncResult.status == Office.AsyncResultStatus.Failed) {
-             write('Action failed. Error: ' + asyncResult.error.message);
-             } else {
-             write('Added new binding with type: ' + asyncResult.value.type + ' and id: ' + asyncResult.value.id);
+                        myBindings.addFromNamedItemAsync(myAddress, "matrix", {id:'myBinding'}, function (asyncResult) {
+                            if (asyncResult.status == Office.AsyncResultStatus.Failed) {
+                                write('Action failed. Error: ' + asyncResult.error.message);
+                            } else {
+                                write('Added new binding with type: ' + asyncResult.value.type + ' and id: ' + asyncResult.value.id);
 
-             function addHandler() {
-             Office.select("bindings#myBinding").addHandlerAsync(
-             Office.EventType.BindingDataChanged, dataChanged);
-             }
+                                function addHandler() {
+                                    Office.select("bindings#myBinding").addHandlerAsync(
+                                        Office.EventType.BindingDataChanged, dataChanged);
+                                }
 
-             addHandler();
-             displayAllBindings();
+                                addHandler();
+                                displayAllBindings();
 
-             }
-             });
-             }
+                            }
+                        });
+                    }
 
-             bindFromPrompt();
+                    bindFromPrompt();
 
-             function displayAllBindings() {
-             Office.context.document.bindings.getAllAsync(function (asyncResult) {
-             var bindingString = '';
-             for (var i in asyncResult.value) {
-             bindingString += asyncResult.value[i].id + '\n';
-             }
-             });
-             }
+                    function displayAllBindings() {
+                        Office.context.document.bindings.getAllAsync(function (asyncResult) {
+                            var bindingString = '';
+                            for (var i in asyncResult.value) {
+                                bindingString += asyncResult.value[i].id + '\n';
+                            }
+                        });
+                    }
 
-             function dataChanged(eventArgs) {
-             window.location = "duplicates.html";
-             }
+                    function dataChanged(eventArgs) {
+                        window.location = "duplicates.html";
+                    }
 
-             // Function that writes to a div with id='message' on the page.
-             function write(message){
-             console.log(message);
-             }
+                    // Function that writes to a div with id='message' on the page.
+                    function write(message){
+                        console.log(message);
+                    }
 
-             });
-             }).catch(function(error) {
-             console.log("Error: " + error);
-             if (error instanceof OfficeExtension.Error) {
-             console.log("Debug info: " + JSON.stringify(error.debugInfo));
-             }
-             });*/
+                });
+            }).catch(function(error) {
+                console.log("Error: " + error);
+                if (error instanceof OfficeExtension.Error) {
+                    console.log("Debug info: " + JSON.stringify(error.debugInfo));
+                }
+            });
 
         });
     };
-
-
-    function highlightHeader() {
-
-        Excel.run(function (ctx) {
-
-            var worksheet = ctx.workbook.worksheets.getActiveWorksheet();
-            var range_all = worksheet.getRange();
-            var range = range_all.getUsedRange();
-
-            range.load('text');
-
-            return ctx.sync().then(function() {
-
-                for (var run = 0; run < range.text[0].length - 1; run++) {
-                    for (var run2 = run + 1; run2 < range.text[0].length; run2++) {
-                        if (range.text[0][run] == range.text[0][run2] && range.text[0][run] != "") {
-                            document.getElementById('showEmbeddedDialog').style.visibility = 'hidden';
-                            highlightContentInWorksheet(worksheet, getCharFromNumber(run) + 1, '#EA7F04');
-                            highlightContentInWorksheet(worksheet, getCharFromNumber(run2) + 1, '#EA7F04');
-                        }
-                    }
-                }
-
-            });
-
-        }).catch(function(error) {
-            console.log("Error: " + error);
-            if (error instanceof OfficeExtension.Error) {
-                console.log("Debug info: " + JSON.stringify(error.debugInfo));
-            }
-        });
-    }
 
 
     function populateCheckboxes() {
@@ -193,8 +145,10 @@ function fuzzyPro() {
 
                 for (var run = 0; run < range.text[0].length - 1; run++) {
                     for (var run2 = run + 1; run2 < range.text[0].length; run2++) {
-                        if (range.text[0][run] == range.text[0][run2] && range.text[0][run] != "") {
+                        if (range.text[0][run] == range.text[0][run2]) {
                             document.getElementById('showEmbeddedDialog').style.visibility = 'visible';
+                            highlightContentInWorksheet(worksheet, getCharFromNumber(run) + 1, '#EA7F04');
+                            highlightContentInWorksheet(worksheet, getCharFromNumber(run2) + 1, '#EA7F04');
                             Office.context.document.settings.set('same_header_duplicates', true);
                         }
                     }
@@ -274,15 +228,9 @@ function fuzzyPro() {
             var worksheet = ctx.workbook.worksheets.getActiveWorksheet();
             var range_all = worksheet.getRange();
             var range = range_all.getUsedRange();
-            var firstCol = range.getRow(1);
-            var lastCol = range.getLastColumn();
 
             range.load('address');
             range.load('text');
-            worksheet.load('name');
-            firstCol.load('address');
-            lastCol.load('address');
-
             return ctx.sync().then(function() {
 
                 var columns_to_check = [];
@@ -306,129 +254,40 @@ function fuzzyPro() {
                     strings_to_sort.push(this_row);
                 }
 
-                function Comparator(a, b) {
-                    for (var i = 0; i < checked_checkboxes.length; i++){
-                        if (a[i][0] < b[i][0]) return -1;
-                        if (a[i][0] > b[i][0]) return 1;
-                    }
-                    return 0;
-                }
 
-                strings_to_sort.sort(Comparator);
-                var duplicates = [];
+                // call to API
 
-                function arraysEqual(a, b) {
-                    if (a === b) return true;
-                    if (a == null || b == null) return false;
-                    if (a.length != b.length) return false;
+                $.post( "https://localhost:8100/", { data: strings_to_sort })
+                    .done(function( data ) {
+                        // highlight dupes
+                        console.log("Data: " + data + "\nStatus: " + status);
 
-                    // If you don't care about the order of the elements inside
-                    // the array, you should sort both arrays here.
+                        Excel.run(function (ctx) {
 
-                    for (var i = 0; i < a.length; ++i) {
-                        if (a[i][0] !== b[i][0]) return false;
-                    }
-                    return true;
-                }
+                            var dupe_worksheet = ctx.workbook.worksheets.getActiveWorksheet();
+                            var dupe_range_all = dupe_worksheet.getRange();
+                            var dupe_range = dupe_range_all.getUsedRange();
 
-                var dup_count = 1;
-                var dup_index = 1;
-                for (var o = 1; o < strings_to_sort.length; o++){
-                    if (arraysEqual(strings_to_sort[o], strings_to_sort[o - 1])){
-                        if (dup_count == 1) {
-                            duplicates.push([strings_to_sort[o], dup_index]);
-                            duplicates.push([strings_to_sort[o - 1], dup_index]);
-                            dup_count += 1;
-                        }
-                        else {
-                            duplicates.push([strings_to_sort[o], dup_index]);
-                        }
-                    }
-                    else {
-                        dup_count = 1;
-                        dup_index += 1;
-                    }
-                }
+                            dupe_range.load('address');
+                            dupe_range.load('text');
+                            return ctx.sync().then(function() {
+                                var color = "#EA7F04";
+                                for (var m = 0; m < data.length; m++){
+                                    if (m > 0 && m % 2 == 0){
+                                        // generate new random color
+                                        color = getRandomColor();
+                                    }
 
-                function colorDup(duplicates_input, int) {
-                    var color = "#EA7F04";
-                    for (var m = 0; m < duplicates_input.length; m++){
-                        if (m > 0 && duplicates_input[m][1] != duplicates_input[m-1][1]){
-                            // generate new random color
-                            color = getRandomColor();
-                        }
 
-                        for (var n = 0; n < duplicates_input[m].length; n++){
-                            for (var o = 0; o < duplicates_input[m][n].length; o++) {
-                                highlightContentInWorksheet(worksheet, duplicates_input[m][n][o][int], color);
-                            }
+                                    highlightCellInWorksheet(dupe_worksheet, data[m][0], color);
+                                    highlightCellInWorksheet(dupe_worksheet, data[m][1], color);
+                                }
+                            });
 
-                        }
-                    }
-                }
+                            // window.location = "duplicates.html";
+                        });
+                    });
 
-                if(document.getElementById('duplicatesort').checked == false) {
-                    colorDup(duplicates, 1);
-                }
-                else {
-
-                    var dup_length = duplicates.length;
-                    var sheet_row = 2;
-
-                    var row_numbers = [];
-                    for (var run = 0; run < duplicates.length; run++) {
-                        row_numbers.push(duplicates[run][0][0][2]);
-                    }
-
-                    var text = [];
-                    var color_check = []
-                    var data_index = 0;
-                    for (var run = 0; run < row_numbers.length; run++) {
-                        data_index = row_numbers[run] - 1;
-                        text.push(range.text[data_index]);
-                        color_check.push(duplicates[run][1]);
-                    }
-
-                    for (var run = 1; run < range.text.length; run ++) {
-                        var check = 0;
-                        for (var k = 0; k < row_numbers.length; k++) {
-                            if((run + 1) == row_numbers[k]) {
-                                check = 1;
-                            }
-                        }
-                        if (check == 0) {
-                            text.push(range.text[run]);
-                        }
-                    }
-
-                    var color = '#EA7F04';
-
-                    var start_col = firstCol.address.substring(firstCol.address.indexOf("!") + 1, firstCol.address.indexOf(":"));
-                    var end_col = lastCol.address.substring(lastCol.address.indexOf(":") + 1);
-
-                    addContentNew(worksheet.name, start_col + ":" + end_col, text);
-
-                    for (var row = 0; row < text.length; row++) {
-                        for(var col = 0; col < range.text[0].length; col++) {
-                            var columnchar = getCharFromNumber(col)
-                            addContentToWorksheet(worksheet, columnchar + sheet_row, text[row][col])
-                            if (sheet_row < (row_numbers.length + 2)) {
-                                if (row > 0 && color_check[row] == color_check[row - 1])
-                                    highlightContentInWorksheet(worksheet, columnchar + sheet_row ,color)
-                            }
-                        }
-                        sheet_row += 1;
-                    }
-                }
-
-                var txt = document.createElement("p");
-                txt.className = "ms-font-xs ms-embedded-dialog__content__text";
-                txt.innerHTML = "PrepJet found " + duplicates.length + " duplicate rows."
-                document.getElementById('resultText').appendChild(txt);
-
-                document.getElementById('resultDialog').style.visibility = 'visible';
-
-                //window.location = "duplicates.html";
 
             });
 
