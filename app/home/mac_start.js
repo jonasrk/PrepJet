28592e35df67a4408cd35afb1b3ba342b59b5ebd
Corinne <corinne.petroschke@patya.de>
2016-08-11 15:53:00 +0200

    mac support

---
+++ app/home/mac_start.js
@@ -0,0 +1,25 @@
+(function () {
+    // 'use strict';
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        jQuery(document).ready(function () {
+
+            app.initialize();
+
+            /*$('#splitValHome').click(home);
+            $('#addColHome').click(home);
+            $('#extractValHome').click(home);
+            $('#trimSpacesHome').click(home);
+            $('#detDupHome').click(home);
+            $('#detOutHome').click(home);
+            $('#valRuleHome').click(home);
+            $('#harmonizeHome').click(home);
+            $('#undoHelpHome').click(home);
+            $('#tableHeaderHome').click(home);
+            $('#customIncon').click(home);*/
+
+        });
+    };
+
+})();
